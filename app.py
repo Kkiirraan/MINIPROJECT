2f04a3c07c580a19d1e1a91c7cb44af6a16bff14
kiran <kiranvijayanv@gmail.com>
2025-01-03 21:46:09 +0530

    name

--- app.py
+++ app.py
@@ -18,12 +18,12 @@ result=0
 
 
 app=Flask(__name__)
-app.secret_key = 'kiran'   
+app.secret_key = 'something'   
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SESSION_REFRESH_EACH_REQUEST'] = True
 
-app.config['MAIL_SERVER']="smtp.gmail.com"
-app.config['MAIL_PORT']=587
+app.config['MAIL_SERVER']=""
+app.config['MAIL_PORT']=000
 app.config['MAIL_USE_TLS']=True
 app.config['MAIL_USERNAME']=""
 app.config['MAIL_PASSWORD']=""
